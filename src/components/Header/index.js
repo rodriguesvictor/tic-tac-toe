a2bb42daf060c804b04798809bb20da8122635ff
Victor Rodrigues <Victordarkiin@gmail.com>
2023-04-29 18:54:20 +0100

    Atualizando nome e imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../../objects/Logo';
 import Menu from '../../objects/Menu';
-import Sobre from '../../objects/Sobre';
+import Sobre from '../../objects/About';
 import './styles.css';
 
 const Header = () => {
