631692f27203440a9f03f05823570938d4b9fe9b
Victor Rodrigues <Victordarkiin@gmail.com>
2023-04-29 19:40:04 +0100

    Criação do object Player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Card from "./objects/Card";
+import Player from "./objects/Player";
 
 function App() {
 	return (
 		<>
 		<Header />
-		<Card /> 
+		<Card />
+		<Player /> 
 		</>
 	);
 }
