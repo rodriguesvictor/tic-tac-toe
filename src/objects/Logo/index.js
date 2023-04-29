2934042351b5254205023b4edaf63868d2aff99f
Victor Rodrigues <Victordarkiin@gmail.com>
2023-04-29 18:43:43 +0100

    atualização em Logo

--- src/objects/Logo/index.js
+++ src/objects/Logo/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LogoCollab from '../../img/logo.png';
+
 const Logo = () => {
   return (
     <img src={LogoCollab} className='logo-collab' alt='Logo da CollabCode' />
